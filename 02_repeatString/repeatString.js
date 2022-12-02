b269c0f4d6d1590823d26a850292b7ca47a46a4a
tommibish <74586216+tommimaki@users.noreply.github.com>
2022-12-02 16:31:33 -0500

    exc 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  str = "";
+  let numero = num;
+    if(num < 0){
+        return 'ERROR'
+    } else{
+  for (i = 1; i <= num; i++) {
+    str += string;
+  } 
+  return str;
+}
 };
 
 // Do not edit below this line
